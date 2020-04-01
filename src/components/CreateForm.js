97cdcb27d72f43180a20d5a69261da8f135cfd95
sienna1223 <pey831223@naver.com>
2020-04-01 22:34:03 +0900

    TODOLIST 비구조화

--- src/components/CreateForm.js
+++ src/components/CreateForm.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './CreateForm.css';
 
 class CreateForm extends Component {
+
+
 	render() {
 		return (
 			<div className="CreateForm">
-				<form className="form_container">
-					<input placeholder="something to do?" />
+				<form className="form_container" >
+					<input value={'GKGKGK'} placeholder="something to do?" />
 					<button>추가</button>
 				</form>
 			</div>
